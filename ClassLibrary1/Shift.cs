8c0ad8368202025adb0babec3e1ea04c0cae9be9
Alex Bystrov <alex29081996@yandex.ru>
2023-04-20 21:50:34 +0300

    Add Shift

--- ClassLibrary1/Shift.cs
+++ ClassLibrary1/Shift.cs
@@ -2,9 +2,11 @@
 {
     public class Shift //https://docs.google.com/document/d/1QSQdwb3s6IOh4avDr_-vpiYnII_80yUAVL3H_JkLNXQ/edit?usp=share_link
     {
-        public Shift()
-        {
+        public string Name { get; }
 
+        public Shift(string name)
+        {
+            Name = name;
         }
     }
 }
